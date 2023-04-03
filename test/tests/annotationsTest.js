97beca202ddd80f8a8ce9b5f04d1829bedef9587
Frank Bennett <biercenator@gmail.com>
2023-04-03 11:10:30 +0900

    Revert hacked test to upstream value

--- test/tests/annotationsTest.js
+++ test/tests/annotationsTest.js
@@ -481,6 +481,6 @@ describe("Create a note from annotations from multiple items and attachments", f
 		// This yields 14 in Jurism. I have been unable to figure
 		// out what causes the discrepancy, but this seems to work
 		// fine in the running client.
-		assert.equal(note.note.split('zotero.org').length - 1, 14);
+		assert.equal(note.note.split('zotero.org').length - 1, 16);
 	});
 });
