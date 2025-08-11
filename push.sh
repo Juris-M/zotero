f42beaf64362c039774ba78302a8a4ce716a79d6
Frank Bennett <biercenator@gmail.com>
2025-08-11 15:33:33 +0900

    Add node version check to push.sh

--- push.sh
+++ push.sh
@@ -13,8 +13,15 @@ if [ "" = "$1" ]; then
   echo Set branch name as first argument
   exit 1
 fi
-
 BRANCH="$1"
+
+# Check node version
+VERSION=$(node --version)
+if [ "$VERSION" != "v16.20.0" ]; then
+    echo Node $VERSION detected, but the Jurism 6 build chain wants node v16.20.0.
+    echo \(To juggle multiple node version nvm is your friend.\)
+   exit 0
+fi
 
 SOURCE_REPO_URL="https://github.com/Juris-M/zotero"
 CI_ZIP="ci/client"
@@ -35,13 +42,18 @@ git checkout $HASH
 # Very slightly fancy here
 ##
 
-dialog --title "Push code for client build" --yesno "Do you trust the content of\nthis ./build directory?"  8 35;
+set +e
+dialog --no-lines --title "Push code for client build" --yesno "Do you trust the content of\nthis ./build directory?"  8 35;
 
 if [ "$?" == 0 ]; then
     REBUILD=0
 else
     REBUILD=1
 fi
+
+set -e
+
+clear
 
 echo -n Okay .
 sleep 2
@@ -50,12 +62,16 @@ sleep 2
 echo -n .
 sleep 2
 
-clear
+echo All righty then.
 
 if [ $REBUILD -eq 1 ]; then
+    echo Rebuilding from scratch. This will take some time.
+    sleep 2
     rm -fR build
     rm .signatures.json
     node ./scripts/build.js
+else
+    echo Trusting the current build. Pushing to remote repo.
 fi
 
 ##
